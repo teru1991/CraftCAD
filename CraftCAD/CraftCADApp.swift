2ef809756af050186e1dd9791f7f8d34fbd2560a
teru1991 <teltel1991@gmail.com>
2025-01-30 21:19:35 +0900

    Initial Commit

---
+++ CraftCAD/CraftCADApp.swift
@@ -0,0 +1,17 @@
+//
+//  CraftCADApp.swift
+//  CraftCAD
+//
+//  Created by 板橋慶治 on 2025/01/30.
+//
+
+import SwiftUI
+
+@main
+struct CraftCADApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CraftCADTests/CraftCADTests.swift
@@ -0,0 +1,17 @@
+//
+//  CraftCADTests.swift
+//  CraftCADTests
+//
+//  Created by 板橋慶治 on 2025/01/30.
+//
+
+import Testing
+@testable import CraftCAD
+
+struct CraftCADTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
